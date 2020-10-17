3f53238bd3d316c4f1db71ab73b58802cfb8e3ef
Wouter <wouterdebot@hotmail.com>
2020-10-17 18:12:24 +0200

    Created repository with proper folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project done as part of the IBM Data Science Professional Certificate',
+    author='Wouter',
+    license='MIT',
+)
